5467c3a25827d88106205b5d118c6076252f3882
heroinedor@gmail.com <heroinedor@gmail.com@3d52d145-c720-0410-8599-1d44e997c4c1>
2011-06-23 09:23:14 +0000

    first commit

---
+++ trunk/JobApplication/src/org/jobapplication/statistics/CompanyStat.java
@@ -0,0 +1,56 @@
+package org.jobapplication.statistics;
+
+public class CompanyStat {
+	protected String application;
+	protected String answer;
+	protected String interview;
+	protected String responseTime;
+	/**
+	 * @return the application
+	 */
+	public String getApplication() {
+		return application;
+	}
+	/**
+	 * @param application the application to set
+	 */
+	public void setApplication(String application) {
+		this.application = application;
+	}
+	/**
+	 * @return the answer
+	 */
+	public String getAnswer() {
+		return answer;
+	}
+	/**
+	 * @param answer the answer to set
+	 */
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	/**
+	 * @return the interview
+	 */
+	public String getInterview() {
+		return interview;
+	}
+	/**
+	 * @param interview the interview to set
+	 */
+	public void setInterview(String interview) {
+		this.interview = interview;
+	}
+	/**
+	 * @return the responseTime
+	 */
+	public String getResponseTime() {
+		return responseTime;
+	}
+	/**
+	 * @param responseTime the responseTime to set
+	 */
+	public void setResponseTime(String responseTime) {
+		this.responseTime = responseTime;
+	}
+}
